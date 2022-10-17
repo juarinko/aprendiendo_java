cc74e5e8f461e95e0f59991a403368cadabfedb6
jgomez05 <72884838+jgomez05@users.noreply.github.com>
2022-10-17 15:19:05 +0100

    change of message on System.out.println line 3

--- src/AprendiendoJava.java
+++ src/AprendiendoJava.java
@@ -1,5 +1,8 @@
+import com.sun.nio.sctp.SctpSocketOption;
+
 public class AprendiendoJava {
     public static void main(String[] args) {
-        System.out.println("Hello world, this is my first program!");
+        System.out.println("Hello world, this is my first program! the age is " );
+
     }
 }
---
+++ src/Variables.java
@@ -0,0 +1,19 @@
+public class Variables {
+    public static void main(String[] args) {
+        //comments
+        final double to_inches = 2.4;
+        double age = 38;
+        System.out.println(age);
+        age = 46;
+        System.out.println(age);
+        System.out.println(to_inches);
+        double testing = age / 3;
+        System.out.println(testing);
+
+        double square = Math.sqrt(9);
+        System.out.println("La raiz cuadrada es: " + square);
+
+
+
+    }
+}
