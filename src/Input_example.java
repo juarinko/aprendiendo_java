b07afc060f4e970c5f181a9de19f3a825b561129
Juan.gomez <jgomez224@hotmail.com>
2022-10-17 16:42:24 +0100

    learning how to input in java

--- src/Input_example.java
+++ src/Input_example.java
@@ -9,5 +9,12 @@ public class Input_example {
         int age = input.nextInt();
         System.out.println("Hola " + name + ", tu edad es " + age );
 
+        // the next line is to show how the method printf works
+        double number1 = 3.12345678;
+        // from the expression "%1.2" %1 is required and the 2 is the number of decimals wanted, the same way we could
+        // write "%1.4" and then the result would be 3.1234
+        System.out.printf("%1.2f",number1);
+
+
     }
 }
